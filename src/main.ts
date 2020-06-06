e1a88538d10a254d00e290dc09a2f49ddfed2589
Matti Jokitulppo <melonmanchan@gmail.com>
2020-06-06 13:44:45 +0300

    Work on initial implementation

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,19 @@
-console.log("hello");
+import uuid from 'uuid';
+import redis from 'ioredis';
+import { IncomingWebhook } from '@slack/client';
+
+const client = redis.createClient(process.env.REDIS_URL || undefined);
+const id = uuid.v4();
+const log = (msg: string, ...rest: any) => console.log(`[id:${id}] ${msg}`, rest);
+
+process.on('unhandledRejection', (e) => {
+  console.error(e); // eslint-disable-line
+  throw new Error('Unhandled exception');
+});
+
+const webhooksUrls: Array<string> = (process.env.SLACK_WEBHOOK_URLS || '').split(',');
+const webhooks: Array<IncomingWebhook> = webhooksUrls.map((url) => new IncomingWebhook(url));
+
+async function main() {}
+
+main();
